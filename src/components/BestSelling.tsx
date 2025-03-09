8b684745ef831477e748aff968c6ca79267f0b7e
Animator55 <animator55developer@gmail.com>
2025-03-09 18:07:27 -0300

    updated base "bestselling"block

--- src/components/BestSelling.tsx
+++ src/components/BestSelling.tsx
@@ -1,21 +1,22 @@
 import Product from "./molecule/_Product"
+import "../assets/bestSelling.css"
 
 const prods: { name: string, price: string, image: string }[] = [
 
-    { "image": "", "name": "Samsung Galaxy S25", "price": "799 €" },
-    { "image": "", "name": "Apple iPhone 16 Pro", "price": "1.167 €" },
-    { "image": "", "name": "Apple AirPods 4", "price": "149 €" },
-    { "image": "", "name": "Apple Mac Mini (2024)", "price": "663 €" },
-    { "image": "", "name": "Apple Watch Series 10", "price": "429 €" },
-    { "image": "", "name": "Apple iPad 10,9\" (2024) Wi-Fi (10ª Gen)", "price": "369 €" },
+    { "image": "lightpink", "name": "Samsung Galaxy S25", "price": "799 €" },
+    { "image": "lightyellow", "name": "Apple iPhone 16 Pro", "price": "1.167 €" },
+    { "image": "lime", "name": "Apple AirPods 4", "price": "149 €" },
+    { "image": "cyan", "name": "Apple Mac Mini (2024)", "price": "663 €" },
+    { "image": "lightblue", "name": "Apple Watch Series 10", "price": "429 €" },
+    { "image": "lightgray", "name": "Apple iPad 10,9\" (2024) Wi-Fi (10ª Gen)", "price": "369 €" },
 
 ]
 
 export default function BestSelling() {
     /// mandatory
-    return <section className="block">
+    return <section className="block --no_padding_h">
         <h3>Best Selling</h3>
-        <ul>
+        <ul className="mandatory_ul">
             {prods.map(el => {
                 return <Product key={Math.random()} el={el} />
             })}
