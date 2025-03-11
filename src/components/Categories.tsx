2b5a53d768867ef9c8d1e2062669c474c69e70e3
Nahuel Ibarra <92542799+Animator55@users.noreply.github.com>
2025-03-11 12:51:33 -0300

    added images

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,10 +1,14 @@
 import "../assets/categories.css"
+import image1 from "../assets/images/phone-main.jpg"
+import image2 from "../assets/images/laptop-main.jpg"
+import image3 from "../assets/images/head-main.jpg"
+import image4 from "../assets/images/watch-main.jpg"
 
 const categories : {name: string, image: string}[]=[
-    {name: "Smart Phone", image: "lightpink"},
-    {name: "Laptop", image: "lightblue"},
-    {name: "Headphones", image: "gray"},
-    {name: "Smart Watch", image: "lightgray"},
+    {name: "Smart Phone", image: image1},
+    {name: "Laptop", image: image2},
+    {name: "Headphones", image: image3},
+    {name: "Smart Watch", image: image4},
 ]
 
 
@@ -16,8 +20,8 @@ export default function Categories (){
                 className="categories_block"
             >
                 <img 
-                style={{backgroundColor: el.image}}
-                // src={el.image} 
+                // style={{backgroundColor: el.image}}
+                src={el.image} 
                 alt="" 
                 />
                 <h3>{el.name}</h3>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,16 @@ import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "../assets/header.css"
+import image1 from "../assets/images/phone-1.jpg"
+import image2 from "../assets/images/pad-1.jpg"
+import image3 from "../assets/images/head-1.jpg"
+import image4 from "../assets/images/phone-2.jpg"
 
 const clientsList: { image: any, name: string, description: string, price: string }[] = [
-    { "image": "white", "name": "Apple iPhone 16 Pro", "description": "Smartphone de 128GB con pantalla Super Retina XDR de 6,1 pulgadas, chip A18 Bionic y sistema de cámara Pro.", "price": "1.167 €" },
-    { "image": "white", "name": "Apple iPad 10,9\" (2024) Wi-Fi (10ª Gen)", "description": "Tablet con pantalla de 10,9 pulgadas, chip A14 Bionic y 64GB de almacenamiento.", "price": "369 €" },
-    { "image": "white", "name": "Apple AirPods 4", "description": "Auriculares inalámbricos Bluetooth con estuche de carga y cancelación activa de ruido.", "price": "149 €" },
-    { "image": "white", "name": "Samsung Galaxy S25", "description": "Smartphone con pantalla AMOLED de 6,2 pulgadas, procesador Exynos 2200 y cámara triple de 50MP.", "price": "799 €" },
+    { "image": image1, "name": "Apple iPhone 16 Pro", "description": "Smartphone de 128GB con pantalla Super Retina XDR de 6,1 pulgadas, chip A18 Bionic y sistema de cámara Pro.", "price": "1.167 €" },
+    { "image": image2, "name": "Apple iPad 10,9\" (2024) Wi-Fi (10ª Gen)", "description": "Tablet con pantalla de 10,9 pulgadas, chip A14 Bionic y 64GB de almacenamiento.", "price": "369 €" },
+    { "image": image3, "name": "Apple AirPods 4", "description": "Auriculares inalámbricos Bluetooth con estuche de carga y cancelación activa de ruido.", "price": "149 €" },
+    { "image": image4, "name": "Samsung Galaxy S25", "description": "Smartphone con pantalla AMOLED de 6,2 pulgadas, procesador Exynos 2200 y cámara triple de 50MP.", "price": "799 €" },
 ]
 
 
@@ -19,9 +23,10 @@ export default function Header() {
         const quoteData = clientsList[index]
         return <div className="header-item">
             <img
-                style={{backgroundColor: quoteData.image}}
-                // src={quoteData.image}
+                // style={{backgroundColor: quoteData.image}}
+                src={quoteData.image}
             />
+            <div className="header_gradient"></div>
             <div className="header-content">
                 <h3 className="header-title">{quoteData.name}</h3>
                 <p className="header-description">{quoteData.description}</p>
